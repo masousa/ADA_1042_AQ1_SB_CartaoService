20aea524c5146b48dddb6595f8e4ea85c72c250c
vinicius.shk <vinicius.shk@gmail.com>
2023-08-30 14:09:03 -0300

    feat(EnderecoRequest): Atributos
    Adiciona os atributos pertinentes ao Endereco no dto EnderecoRequest

--- src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/payloads/request/EnderecoRequest.java
+++ src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/payloads/request/EnderecoRequest.java
@@ -1,4 +1,17 @@
 package tech.ada.bootcamp.arquitetura.cartaoservice.payloads.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EnderecoRequest {
+  private Long identificador;
+  private String cep;
+  private String rua;
+  private String bairro;
+  private String cidade;
+  private String estado;
+  private String complemento;
+  private String numero;
 }

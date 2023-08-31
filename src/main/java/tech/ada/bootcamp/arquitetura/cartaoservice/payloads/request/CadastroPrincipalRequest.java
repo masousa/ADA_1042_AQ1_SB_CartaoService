c15d4309429fdb6f2686147efddfb8bf5a572a1c
Daniel Justo <justodaniel29@gmail.com>
2023-08-31 18:25:17 -0300

    refactor: incluindo validacoes nos DTOs

--- src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/payloads/request/CadastroPrincipalRequest.java
+++ src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/payloads/request/CadastroPrincipalRequest.java
@@ -1,8 +1,26 @@
 package tech.ada.bootcamp.arquitetura.cartaoservice.payloads.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
 import tech.ada.bootcamp.arquitetura.cartaoservice.payloads.TipoCartao;
 
 import java.util.List;
 
-public record CadastroPrincipalRequest (String identificador, String nome, EnderecoRequest endereco, TipoCartao tipoCartao){
+public record CadastroPrincipalRequest (
+
+        //@CPF
+        @NotBlank
+        @Pattern(regexp = "^\\d{11}$")
+        String identificador,
+        @NotBlank
+        String nome,
+        @NotNull
+        @Valid
+        EnderecoRequest endereco,
+        @NotNull
+        TipoCartao tipoCartao){
 }
